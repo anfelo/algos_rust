50207361a4bc473d3afdd046de9562be8c3b60c1
anfelo <af.osorio1341@gmail.com>
2023-06-30 07:28:09 +0200

    add another solution of bubblesort

--- src/bubblesort.rs
+++ src/bubblesort.rs
@@ -1,13 +1,22 @@
-pub fn bubblesort(arr: &mut Vec<i32>) {
+pub fn bubblesort_swapped(arr: &mut Vec<i32>) {
     let mut swapped = true;
 
     while swapped {
         swapped = false;
-
         for i in 0..arr.len() - 1 {
             if arr[i] > arr[i + 1] {
                 arr.swap(i, i + 1);
-                swapped = true;
+                swapped = true
+            }
+        }
+    }
+}
+
+pub fn bubblesort(arr: &mut Vec<i32>) {
+    for i in 0..arr.len() {
+        for j in 0..arr.len() - 1 - i {
+            if arr[j] > arr[j + 1] {
+                arr.swap(j, j + 1);
             }
         }
     }
@@ -29,7 +38,7 @@ mod tests {
             expected: vec![4, 5, 7, 9, 10, 43, 45, 57, 91],
         };
 
-        bubblesort(&mut test_case.input);
+        bubblesort_swapped(&mut test_case.input);
 
         assert_eq!(test_case.input, test_case.expected);
     }
