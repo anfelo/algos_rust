8c03eb7a307c2e2a75ad553c4806b124d531deeb
anfelo <af.osorio1341@gmail.com>
2023-08-10 07:17:36 +0200

    Add pop method to linkedlist stack

--- src/data_structures/linkedlist.rs
+++ src/data_structures/linkedlist.rs
@@ -29,6 +29,26 @@ impl List {
 
         self.head = Link::More(new_node);
     }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
+}
+
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+        }
+    }
 }
 
 #[cfg(test)]
@@ -55,5 +75,20 @@ mod tests {
                 next: Link::Empty
             }))
         );
+    }
+
+    #[test]
+    fn it_pops_the_first_elem_from_list() {
+        let mut list = List::new();
+        list.push(42);
+
+        let elem1 = list.pop();
+
+        assert_eq!(list.head, Link::Empty);
+        assert_eq!(elem1, Some(42));
+
+        let elem2 = list.pop();
+
+        assert_eq!(elem2, None);
     }
 }
